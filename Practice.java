d7dda887507cb0335b70bddfed979ee10cc746e3
ssun-zip <71378709+ssun-zip@users.noreply.github.com>
2020-09-22 18:15:44 +0900

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -406,4 +406,21 @@ public class Practice {
 		System.out.println("문장에서 발견된 n의 개수 " + n);
 	}
 	
+	//#19 continue 문 공백의 갯수
+	public static void continueTest2() {
+		String s = "no news is good news";
+	
+		int n = 0;
+		for(int i = 0; i < s.length(); i++) {
+			//n이 나오는 회수를 센다
+			if(s.charAt(i)!= ' ') {
+				continue;
+				// 다시 for loop돌리기
+			}
+			//n의 개수를 하나 증가한다.
+			n++;		
+		}
+		
+		System.out.println("문장에서 발견된 공백의 개수 " + n);
+	}	
 }
